9063d979a0d5728cd2fbe9d73bbe2559d28ec57f
Julia <j.semeniuk.v@gmail.com>
2020-12-08 15:12:46 +0200

    create adding users part 2

--- src/Container.jsx
+++ src/Container.jsx
@@ -4,6 +4,7 @@ export default class DataBase extends React.Component {
     super(props);
     this.state = {
       users: [{ name: "", match: "", excludes: [], pass: "" }],
+      name: "",
     };
   }
 
@@ -13,22 +14,23 @@ export default class DataBase extends React.Component {
   };
 
   onClickCreateNewUser = () => {
-    const users = this.state;
-    const currenrUsers = users.slice();
+    const { users } = this.state;
+    const currentUsers = users.slice();
     const newUser = {
       name: this.state.name,
       match: null,
       excludes: [],
       pass: "1111",
     };
-    currenrUsers.push(newUser);
+    currentUsers.push(newUser);
     this.setState({
-      users: currenrUsers,
+      users: currentUsers,
       name: "",
     });
   };
 
   render() {
+    const { users } = this.state;
     return (
       <div>
         <div>
@@ -37,6 +39,7 @@ export default class DataBase extends React.Component {
             onChange={this.onChangeUserName}
             type="text"
             placeholder="name"
+            value={this.state.name}
           ></input>
           <br />
           <input
@@ -45,7 +48,26 @@ export default class DataBase extends React.Component {
             value="Create"
           ></input>
         </div>
-        <div></div>
+        <div>
+          <table>
+            <tr>
+              <td>Name</td>
+              <td>Match</td>
+              <td>Excludes</td>
+              <td>Password</td>
+            </tr>
+            {users.map((value) => {
+              return (
+                <tr>
+                  <td>{value.name}</td>
+                  <td>{value.match}</td>
+                  <td>{value.excludes}</td>
+                  <td>{value.pass}</td>
+                </tr>
+              );
+            })}
+          </table>
+        </div>
       </div>
     );
   }
