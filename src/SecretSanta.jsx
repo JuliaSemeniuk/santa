e543e6ef5b9bc718da200646260a1e65ec4f9d64
Julia <j.semeniuk.v@gmail.com>
2020-12-08 12:20:51 +0200

    add state

--- src/SecretSanta.jsx
+++ src/SecretSanta.jsx
@@ -1,6 +1,20 @@
 import React from "react";
 
 export default class SecretSanta extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      users = [
+        {name: 'Andriy', password: 'fylhsq', match: ['Pasha', 'Masha', 'Anton', 'Julia'], exlude: 'Tania'},
+        {name: 'Tania',  password: 'nfyzey', match: ['Pasha', 'Masha', 'Anton', 'Julia'], exlude: 'Andriy'},
+        {name: 'Pasha',  password: 'gfifvf', match: ['Andriy', 'Tania', 'Anton', 'Julia'], exlude: 'Masha'},
+        {name: 'Masha',  password: 'vfifnj', match: ['Andriy', 'Tania', 'Anton', 'Julia'], exlude: 'Pasha'},
+        {name: 'Anton',  password: 'fynjyc', match: ['Pasha', 'Masha', 'Andriy', 'Tania'], exlude: 'Julia'},
+        {name: 'Julia',  password: 'lekszc', match: ['Pasha', 'Masha', 'Andriy', 'Tania'], exlude: 'Anton'},
+      ]
+    }
+  }
   render() {
     return <div></div>;
   }
